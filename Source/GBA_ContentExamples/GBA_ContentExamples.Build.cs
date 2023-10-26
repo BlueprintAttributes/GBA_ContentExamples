66b558aa6b247d705a7e9e71adfdad2b9ff5089b
Mickael Daniel <daniel.mickael@gmail.com>
2023-10-26 15:41:36 +0200

    Move clamping WBP into their own folder

--- Source/GBA_ContentExamples/GBA_ContentExamples.Build.cs
+++ Source/GBA_ContentExamples/GBA_ContentExamples.Build.cs
@@ -8,16 +8,23 @@ public class GBA_ContentExamples : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			
+			"GameplayAbilities",
+			"GameplayTags",
+			"GameplayTasks",
+			
+			// "BlueprintAttributes"
+		});
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+			
+		});
 	}
 }
